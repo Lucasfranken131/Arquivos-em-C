de1bc90b5f0bd5e9101f433dbe98e7a29d95e0bb
Lucas Franco <105955764+Lucasfranken131@users.noreply.github.com>
2023-03-01 09:08:27 -0300

    Calcular

---
+++ main.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+//Lucas Franco
+int main()
+{
+    float var1,var2;
+    float var3;
+    int escolha;
+    printf("1 - Adicao\n2 - Subtracao\n3 - Multiplicacao\n4 - Divisao\nOperacao: ");
+    scanf("%d",&escolha);
+    printf("Insira o primeiro numero: ");
+    scanf("%f",&var1);
+    printf("Insira o segundo numero: ");
+    scanf("%f",&var2);
+    if(escolha == 1){
+        printf("O resultado e: %f",var1+var2);
+    }
+    else if(escolha == 2){
+        printf("O resultado e: %f",var1-var2);
+    }
+    else if(escolha == 3){
+        printf("O resultado e: %f",var1*var2);
+    }
+    else if(escolha == 4){
+        var3 = var1/var2;
+        printf("O resultado e: %f",var3);
+    }
+    else{
+        printf("Vai cagar");
+    }
+    return 0;
+}
